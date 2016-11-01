8b0aca4884cd4f7e8cce7c769ef9da66494b4499
Connor Kiernan <connorkiernan@gmail.com>
2016-11-01 18:27:58 -0400

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,37 @@
+document.write(
+  "<div id='footer'>\
+    <ul>\
+      <li id='contact'>\
+        Contact Us\
+      </li>\
+      <li id='github'>\
+        <a href='https://github.com/cwruRobotics' target=\"_blank\">\
+          <img alt='GitHub' src='res/footer/github-logo.svg' />\
+        </a>\
+      </li>\
+      <li id='facebook'>\
+        <a href='https://www.facebook.com/cwrurobotics' target=\"_blank\">\
+          <img alt='facebook' src='res/footer/facebook-logo.svg' />\
+        </a>\
+      </li>\
+      <li id='twitter'>\
+        <a href='https://twitter.com/cwrubotix' target=\"_blank\">\
+          <img alt='twitter' src='res/footer/twitter-logo.svg' />\
+        </a>\
+      </li>\
+      <li id='youtube'>\
+        <a href='https://www.youtube.com/channel/UCFxYzjLEY09YFLx_UKSINIw' target=\"_blank\">\
+          <img alt='YouTube' src='res/footer/youtube-icon.svg' />\
+        </a>\
+      </li>\
+	  <li id='email'>\
+        <a href='mailto:robotics-exec@case.edu'>\
+          <img alt='YouTube' src='res/footer/email-icon.svg' />\
+        </a>\
+      </li>\
+    </ul>\
+    <p id='copyright'>\
+      Copyright © 2016 CWRUbotix\
+    </p>\
+  </div>"
+);
