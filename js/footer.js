01098177655f2acc72506040e5af8dbeea44718d
Connor Kiernan <connorkiernan@gmail.com>
2017-04-18 17:47:04 -0400

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,42 @@
+document.write(
+  "<div id='footer'>\
+    <ul>\
+      <li id='contact'>\
+        Contact Us\
+      </li>\
+      <li id='github'>\
+        <a href='https://github.com/caseengineerscouncil' target=\"_blank\">\
+          <img alt='GitHub' src='res/footer/github-logo.svg' />\
+        </a>\
+      </li>\
+      <li id='facebook'>\
+        <a href='https://www.facebook.com/caseengineerscouncil' target=\"_blank\">\
+          <img alt='facebook' src='res/footer/facebook-logo.svg' />\
+        </a>\
+      </li>\
+      
+      //If we ever get a Twitter
+      //<li id='twitter'>\
+       // <a href='https://twitter.com/caseengineerscouncil' target=\"_blank\">\
+        //  <img alt='twitter' src='res/footer/twitter-logo.svg' />\
+       // </a>\
+      //</li>\
+      
+      //If we ever get a YouTube channel
+      //<li id='youtube'>\
+        //<a href='https://www.youtube.com/channel/UCFxYzjLEY09YFLx_UKSINIw' target=\"_blank\">\
+          //<img alt='YouTube' src='res/footer/youtube-icon.svg' />\
+        //</a>\
+      //</li>\
+      
+	  <li id='email'>\
+        <a href='mailto:cec-exec@case.edu'>\
+          <img alt='YouTube' src='res/footer/email-icon.svg' />\
+        </a>\
+      </li>\
+    </ul>\
+    <p id='copyright'>\
+      Copyright © 2016 Case Engineers Council\
+    </p>\
+  </div>"
+);
